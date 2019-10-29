e0f27e50ee9d92763bf7048ef05c64ac905c9312
Salim <salimryad1@gmail.com>
2019-10-29 11:09:32 +0100

    Using yield in continuation

---
+++ lab6/src/test/java/fr/umlv/java/inside/AppTest.java
@@ -0,0 +1,28 @@
+package fr.umlv.java.inside;
+
+/*
+
+public class AppTest 
+    extends TestCase
+{
+
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+}
+
+ */
